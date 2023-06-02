918e8e5da014285147be5efd1f46da327c748fc2
arifullinrd <arifullinrd@lad24.ru>
2023-06-02 20:53:44 +0300

    Состояние компонентов

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+
+const App = () => {
+    const [count, setCount] = useState(0);
+    const [inputValue, setInputValue] = useState();
+
+    const onClick = () => {
+        setCount(state=> state + 1);
+    }
+
+    const onChangeInput = (event)=>{
+        setInputValue(event.target.value);
+    }
+
+    return(
+        <div>
+            <p>{count}</p>
+            <button onClick={onClick}>add 1</button> <br/><br/>
+           <input value={inputValue} onChange={onChangeInput}/>
+            <p>{inputValue}</p>
+        </div>
+    )
+}
+
+export default App;
